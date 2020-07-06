5326f8297ab97098d48b14fcc5610432303b554d
Sandeep Sundaram <sansun2111@gmail.com>
2020-07-07 01:09:58 +0530

    Adding address card component

---
+++ src/app/address-card/user.model.ts
@@ -0,0 +1,6 @@
+export class User{
+    name: string;
+    title: string;
+    city:string;
+    phone: string[];
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './address-card/user.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-project';
+   user:User;
+  constructor(){
+    this.user = new User();
+    this.user.name ='Sandeep';
+    this.user.city ='Hyderabad';
+    this.user.title ='Lead';
+    this.user.phone=['9949283691']
+  }
 }
